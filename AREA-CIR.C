cb56043ac042301219b76cea53da10e067ec1ffd
patelpreksha101 <103107189+patelpreksha101@users.noreply.github.com>
2022-04-06 14:24:37 +0530

    Add files via upload

---
+++ AREA-CIR.C
@@ -0,0 +1,13 @@
+//WAP to compute area of circle.
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float r=4;
+float pi=3.14;
+float area;
+clrscr();
+area=pi*r*r;
+printf("area=%f",area);
+getch();
+}
